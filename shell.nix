1f58cb5c7451789b396a06e8e51aa6e66a7849f3
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-09-28 11:53:36 +0200

    some message

--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ stdenv.mkDerivation {
     shellHook = ''
         # make sure no output is done, since direnv fails with direnv: error unmarshal() base64 decoding: illegal base64 data at input byte ?
         npm install @angular/cli > /dev/null 2>&1
+        npm install tsun > /dev/null 2>&1
         # put all linked executables in node_modules on the path
         export PATH=`pwd`/node_modules/.bin:$PATH
         # run a fresh build to ensure node_modules is populated with all necessary dependencies
