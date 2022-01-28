897cb007d45ba5a5d5b7ff7544d05798cd1c2b2e
Gunther Bachmann <gunther.bachmann@itemis.de>
2022-01-28 17:55:21 +0100

    fix angular cli installation through nix shell

--- shell.nix
+++ shell.nix
@@ -1,20 +1,21 @@
 with import <nixpkgs> {};
 
+let
+  angularcli = nodePackages."@angular/cli".overrideAttrs (_: {
+    NG_CLI_ANALYTICS = false;
+  });
+in
 stdenv.mkDerivation {
     name = "magit-demo-web-project-setup";
     buildInputs = [
-        nodejs
-        nodePackages.npm
+        nodejs-12_x
+        angularcli # nodePackages.@angular/cli
         nodePackages.jsonlint
+        nodePackages.tsun
         bashInteractive
         emacs
     ];
     shellHook = ''
-        # make sure no output is done, since direnv fails with direnv: error unmarshal() base64 decoding: illegal base64 data at input byte ?
-        npm install @angular/cli > /dev/null 2>&1
-        npm install tsun > /dev/null 2>&1
-        # put all linked executables in node_modules on the path
-        export PATH=`pwd`/node_modules/.bin:$PATH
         # run a fresh build to ensure node_modules is populated with all necessary dependencies
         # npm run build > /dev/null 2>&1 # too expensive, makes switching a pain
     '';
